0160297581dad5cacb364857acafeca0b96cb304
MatheusMunizera <matheus.munizera@gmail.com>
2022-01-13 19:48:04 -0300

    Property & Data Binding, Directives, Interpolation

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+   widthImg: number = 100;
+  marginImg: number = 2;
+  showImage: boolean = true;
+  private _filterList: string = '';
+
+
+
 
 
 
@@ -18,14 +26,37 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public toggleImage(){
+    this.showImage = !this.showImage;
+  }
   public getEventos(): void{
 
     this.http.get(`https://localhost:5001/api/eventos`).subscribe(
-      Response => this.eventos = Response,
+      Response => {
+        this.eventos = Response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
-
-
   }
+
+  public get filterList(): string{
+    return this._filterList;
+    }
+
+
+  public set filterList(value: string){
+      this._filterList = value;
+      this.eventosFiltrados = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+    }
+
+   filterEvents(by: string): any{
+    by = by.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string;}) => evento.tema.toLocaleLowerCase().indexOf(by) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(by) !== -1
+    )
+  }
+
 
 }
